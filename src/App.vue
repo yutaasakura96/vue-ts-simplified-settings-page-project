e22b951878a425b4b33302c5c3d31079018a40d3
yuta asakura <yuta.asakura.se@gmail.com>
2024-10-14 08:13:29 +0900

    feat: Defined props and emits with TypeScript types, refine ref and data types
    - Added TypeScript types for props and events in the <script setup> block.
    - Defined `Tab` and `TabKey` interfaces for tab navigation.
    - Narrowed down ref types for better type safety.
    - Improved maintainability by enforcing strict type checking across props and events.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Tab, TabKey } from '@/types';
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: 'General'
+  },
+  {
+    key: 'Notifications',
+    label: 'Notifications'
+  },
+  {
+    key: 'Privacy',
+    label: 'Privacy'
+  }
+];
+
+const currentTab = ref<TabKey>(null);
+</script>
 
 <template>
   <main>Hello</main>
---
+++ src/components/TabLink.vue
@@ -0,0 +1,14 @@
+<template>Link</template>
+
+<script setup lang="ts">
+import type {Tab} from '../types'
+
+defineProps<{
+  currentTab: string;
+  tab: Tab;
+}>();
+
+defineEmits<{
+  click: [];
+
+}>()
